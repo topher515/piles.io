7100d3069e691b80f98ba2b75d6ccfd3cd32cc7a
Chris Wilcox <topher515@gmail.com>
2011-09-18 22:54:42 -0700

    Usage debug

--- static/js/piles.usage.js
+++ static/js/piles.usage.js
@@ -270,7 +270,7 @@ $(function() {
                 self.$el.find('#detailed-storage').slideDown()
             })
             
-            this.$el.find('#detailed-sum #free-progress').progressbar({ value = (this.model.get('this_month_dollars') / this.model.get('freeloaders_this_month_dollars')) })
+            this.$el.find('#detailed-sum #free-progress').progressbar({ value : (this.model.get('this_month_dollars') / this.model.get('freeloaders_this_month_dollars')) })
             
             return this
         }
