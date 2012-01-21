e8c5d5c9121d9137f17789509bb714e32bd2c958
Chris Wilcox <topher515@gmail.com>
2012-01-20 23:04:39 -0800

    try and update bootstrap

--- piles_js/bootstrap.sh
+++ piles_js/bootstrap.sh
@@ -4,9 +4,16 @@ pushd node_modules
 git clone https://github.com/christkv/node-mongodb-native/ mongodb
 git checkout 4c1bda08484cf7913235cae56941e755518a766f
 popd
+npm install coffee-script
 npm install connect
 npm install express
-npm install underscore
+npm install hashlib
+npm install less
+npm install mongodb
+npm install mustache
 npm install socket.io
+npm install stache
+npm install underscore
+npm install vows
 npm install yaml
-sudo npm install -g tamejs
+#sudo npm install -g tamejs
